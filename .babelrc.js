f0fd504cb36715e722e1ca10e3ac5dddfac93690
tizee <33030965+tizee@users.noreply.github.com>
2020-06-14 22:54:05 +0800

    [update] typscript preset config

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,19 @@
-const presets = ['@babel/preset-env', '@babel/preset-typescript'];
+// more on https://babeljs.io/docs/en/presets#preset-options
+const presets = [
+  '@babel/preset-env',
+  [
+    '@babel/preset-typescript',
+    {
+      isTsx: false, // Forcibly enables jsx parsing.
+      //  jsxPragma: 'React', // Replace the function used when compiling JSX expressions
+      allExtensions: false, // Indicates that every file should be parsed as TS or TSX (depending on the isTSX option).
+      allowNamespaces: true, // Enables compilation of TypeScript namespaces.
+      allowDeclareFields: false, // default true in Babel 8
+      onlyRemoveTypeImports: false, // When set to true, the transform will only remove type-only imports (introduced in TypeScript 3.8).
+    },
+  ],
+];
+
 const plugins = ['@babel/plugin-proposal-class-properties'];
 
 const ignore = ['dist', 'src/__tests__'];
